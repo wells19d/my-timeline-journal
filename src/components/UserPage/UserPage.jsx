42cd1093a9b192e4f11eaa4032dc8f13799ff8eb
Aron Wells <wells19d@gmail.com>
2021-06-07 12:55:49 -0500

    note pages

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,11 +6,7 @@ function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-    </div>
+    <>This should be the User Profile Page</>
   );
 }
 
