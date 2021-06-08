f2c04fa33ce9ee9b8dc35d697f99dc1e459a5f0e
Aron Wells <wells19d@gmail.com>
2021-06-08 14:35:11 -0500

    corrected history

--- src/components/3-Main/Main.jsx
+++ src/components/3-Main/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function Main() {
+    const history = useHistory();
+
     return (
         <Router>
             <center>
