dca588e79fed4ed579397396eaa16fa8586e760f
Aron Wells <wells19d@gmail.com>
2021-06-10 15:23:09 -0500

    about and user agreement

--- src/components/2-Registration/UserAgreement.jsx
+++ src/components/2-Registration/UserAgreement.jsx
@@ -3,40 +3,40 @@ import React from 'react';
 function UserAgreement() {
   return (
     <div className='container'>
-      <p>Welcome to My Timeline Journal:</p>
+      <form>
+      Welcome to My Timeline Journal:
 
-        <p>This is a sample USER END AGREEMENT only.</p>
-        
-        <p>END USER LICENSE AGREEMENT:
-        Last updated June 10, 2021</p>
+        This is a sample USER END AGREEMENT only.
 
-        <p>My Timeline Journal is licensed for only under the terms of this license agreement.
+        END USER LICENSE AGREEMENT:
+        Last updated June 10, 2021
+
+        My Timeline Journal is licensed for only under the terms of this license agreement.
         The use of this application an any update thereto (as permitted by this license agreement),
-        Your indicate that you agree to be bound by all the terms and condition of this license agreement.</p>
+        Your indicate that you agree to be bound by all the terms and condition of this license agreement.
 
-        <p>The prties of the license agreement ackknowledge that this license agreement is not bound by any 
-          provisions or oblications with regard to the application, such as any warranty, liability, maintenance and support.</p>
+        The prties of the license agreement ackknowledge that this license agreement is not bound by any 
+          provisions or oblications with regard to the application, such as any warranty, liability, maintenance and support.
 
-        <p>The license agreement may not provide any usage rules for the Application that conflict with outside applications.</p>
+        The license agreement may not provide any usage rules for the Application that conflict with outside applications.
 
-        <p>1: The application</p>
-        <p>My Timeline Journal is a piece oe software created to allow users to make offline journal entries, to help themselves remember and recall envents from their own past. It is solely only used 
-          for entertainmet purposes and to only be used as such. Users may not reverse engineer or modify the application other than what it was intended for.</p>
+        1: The application
+        My Timeline Journal is a piece oe software created to allow users to make offline journal entries, to help themselves remember and recall envents from their own past. It is solely only used 
+          for entertainmet purposes and to only be used as such. Users may not reverse engineer or modify the application other than what it was intended for.
 
-        <p>2: Abuse</p>
-        <p>Any use of this application to impose threats, violence, other illegal activities, will result in account termination and information made available to authorities. Mis-use of this application
-          may result in prosecution to the fullest extent. Any attempt to breach information used on this application will result in termination and legal action.</p>
+        2: Abuse
+        Any use of this application to impose threats, violence, other illegal activities, will result in account termination and information made available to authorities. Mis-use of this application
+          may result in prosecution to the fullest extent. Any attempt to breach information used on this application will result in termination and legal action.
 
-        <p>3: Termination</p>
-        <p>My Timeline Journal reserves the right to inspect all entries that are subject to term violations only. All information will be encoded and encrpyted for user privacy. 
+        3: Termination
+        My Timeline Journal reserves the right to inspect all entries that are subject to term violations only. All information will be encoded and encrpyted for user privacy. 
           All entries will be encrypted and only made available to the user it was intended for,
-           unless the user has violated the use of the application and information pass along to local authorities.</p>
+           unless the user has violated the use of the application and information pass along to local authorities.
 
-        <p>4: Privacy:</p>
-        <p>All entires on My Timeline Journal will be kept in a secure and encrypted system to prevent outside usage. In no way will My Timeline Journal reveal any information uploaded into the system. 
+        4: Privacy:
+        All entires on My Timeline Journal will be kept in a secure and encrypted system to prevent outside usage. In no way will My Timeline Journal reveal any information uploaded into the system. 
           All information will be kept in a confidential and secure section, unless required to by legal requirements.
-        </p>
-
+      </form>
     </div>
   );
 }
--- src/components/9-AboutUs/AboutUs.jsx
+++ src/components/9-AboutUs/AboutUs.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 function AboutUs() {
   return (
-    <center>
-      <>This is the About Us Page</>
-    </center>
+    <form>
+      Ever shared a moment of your past when suddenly you forgot... when, where, or what exactly happened during 
+      that moment? Well for me it's been happening a lot as I've gotten older. In the past I've tried writing down 
+      my thoughts and events in a journal, but as we all know today's world has become a busier place. Having time 
+      to sit and organize your thoughts has become a challenge. Sure, you can use a medium like "Facebook", add some 
+      entries about your day, and then you worry about what others have to say or even think about what you are doing 
+      with your life... suddenly you've become bitter and stop writing.
+      
+      Lately I've been thinking it would be nice if you could have some app program that you could see as a timeline. 
+      You could enter details about your day, maybe a few pictures, and save it to your timeline. Let's say a few days 
+      later you remembered something you wanted to add… Well, you just select that date and make a quick edit. How about 
+      you remembered something from your childhood, and you don't want to forget about it? Not a problem, you still select 
+      the date, write the details, and drop it in. The point of the app is it wouldn't matter what date you wanted to add, 
+      edit, move, or remove, you could easily do so and have access to these memories whenever you wanted.
+    </form>
   );
 }
 
