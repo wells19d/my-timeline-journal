37e5ada52a8711bb551e6904749eb6f48fde01a8
Aron Wells <wells19d@gmail.com>
2021-06-10 15:37:12 -0500

    contact us

--- src/components/8-ContactUs/ContactUs.jsx
+++ src/components/8-ContactUs/ContactUs.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 function ContactUs() {
   return (
+    <div>
     <center>
-      <>This is the Contact Us Page</>
+      <p>This is a feature coming soon, for now please <br /> 
+      <a href="mailto:wells19d@gmail.com?subject=MTJ Assistance">
+ Send an Email</a>!</p>
     </center>
-  );
+    </div>
+  )
 }
 
 export default ContactUs;
