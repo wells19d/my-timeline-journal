58f533ab3c136a9db0d1c4f621ef30238fd949f3
Aron Wells <wells19d@gmail.com>
2021-06-16 09:56:05 -0500

    format

--- src/components/5-Add/Add.jsx
+++ src/components/5-Add/Add.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import TextField from '@material-ui/core/TextField';
-import Swal from 'sweetalert2';
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import TextField from "@material-ui/core/TextField";
+import Swal from "sweetalert2";
 
 function Add() {
-  const [date, setDate] = useState('');
-  const [photo, setPhoto] = useState('');
-  const [entry, setEntry] = useState('');
+  const [date, setDate] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [entry, setEntry] = useState("");
 
   const dispatch = useDispatch();
   const history = useHistory();
 
   useEffect(() => {
     dispatch({
-      type: 'FETCH_ENTRY',
+      type: "FETCH_ENTRY",
     });
   }, []);
 
   const addEntry = (event) => {
     Swal.fire({
-      icon: 'success',
-      title: 'Your post was successful!',
+      icon: "success",
+      title: "Your post was successful!",
       showConfirmButton: false,
-      timer: 1500
-    })
+      timer: 1500,
+    });
     event.preventDefault();
     // console.log(`Added Journal Entry`, { date, photo, entry }); // checking to see what was added to the entry
-    history.push('./view');
+    history.push("./view");
 
     dispatch({
-      type: 'ADD_ENTRY',
+      type: "ADD_ENTRY",
       payload: {
         date: date,
         photo: photo,
@@ -47,9 +47,9 @@ function Add() {
       <center>
         <form>
           <TextField
-            variant='outlined'
-            label='Journal Event Date'
-            type='date'
+            variant="outlined"
+            label="Journal Event Date"
+            type="date"
             value={date}
             onChange={(event) => setDate(event.target.value)}
             required
@@ -59,9 +59,9 @@ function Add() {
           />
           {`\u00A0\u00A0\u00A0\u00A0\u00A0`}
           <TextField
-            variant='outlined'
-            label='Photo URL'
-            type='text'
+            variant="outlined"
+            label="Photo URL"
+            type="text"
             value={photo}
             onChange={(event) => setPhoto(event.target.value)}
             InputLabelProps={{
@@ -71,10 +71,10 @@ function Add() {
           <br />
           <br />
           <TextField
-            variant='outlined'
-            label='Add Entry'
-            type='textarea'
-            style={{ width: '500px' }}
+            variant="outlined"
+            label="Add Entry"
+            type="textarea"
+            style={{ width: "500px" }}
             value={entry}
             multiline
             rows={4}
@@ -87,18 +87,18 @@ function Add() {
           <br />
           <br />
           <Button
-            type='button'
-            className='btn btn_asCancel'
+            type="button"
+            className="btn btn_asCancel"
             onClick={() => {
-              history.push('/main'); // Sends user back to the main page
+              history.push("/main"); // Sends user back to the main page
             }}
           >
             Cancel
           </Button>
           {`\u00A0\u00A0\u00A0\u00A0`}
           <Button
-            type='submit'
-            className='btn btn_asSubmit'
+            type="submit"
+            className="btn btn_asSubmit"
             onClick={(event) => addEntry(event)} // Sends user back to the main page
           >
             Submit
