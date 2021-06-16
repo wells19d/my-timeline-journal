c2500181c1dc49eb8b6003cc73b260ed5f8403be
Aron Wells <wells19d@gmail.com>
2021-06-16 16:19:43 -0500

    layout adjustments

--- src/components/3-Main/Main.jsx
+++ src/components/3-Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Table from '@material-ui/core/Table';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
@@ -18,56 +18,46 @@ function Main() {
   // }, [dispatch]);
 
   return (
+    <center>
+      <Table className="displayTable">
+        <tbody className="tableBody">
+          <tr>
+            <td className="tableCellLeft">
+              <Card className="leftCard">
+                <ul>
 
-        <center>
-          <table className="displayTable">
-            <tbody className="tableBody">
-              <tr>
-                <td className="tableCellLeft">
-                  <Card className="leftCard">
-                  <ul>
-                  {/* <li> Content Here </li> */}
-                </ul>
-                </Card>
-                </td>
-                <td className="tableCellRight">
-                  <Card className="viewCard">
-                    <center>Welcome {user.username}</center>
-                  </Card>
-                  </td>
-              </tr>
-              <tr>
-              <td>{`\u00A0\u00A0\u00A0\u00A0`}</td>
-              <td>{`\u00A0\u00A0\u00A0\u00A0`}</td>
-              </tr>
-            </tbody>
-          </table>
-        </center>
+
+                  {/* <li> Dates Listed Here </li> */}
+
+
+                  </ul>
+              </Card>
+            </td>
+            <td className="tableCellRight">
+              <Card className="viewCard">
+                <center>
+
+
+                  Welcome {user.username}
+                  <p>
+                    <Button type="button" className="btn btn_asLink" onClick={() => { history.push('/add'); }} > Add Entry </Button>
+                    {`\u00A0\u00A0\u00A0\u00A0`}
+                    <Button type="button" className="btn btn_asLink" onClick={() => { history.push('/view'); }} > View Page </Button>
+                  </p>
+
+                  
+                </center>
+              </Card>
+            </td>
+          </tr>
+          <tr>
+            <td>{`\u00A0\u00A0\u00A0\u00A0`}</td>
+            <td>{`\u00A0\u00A0\u00A0\u00A0`}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </center>
   );
 }
 
 export default Main;
-
-/* 
-          Welcome {user.username}
-          <p>
-            <Button
-              type='button'
-              className='btn btn_asLink'
-              onClick={() => {
-                history.push('/add');
-              }}
-            >
-              Add Entry
-            </Button>
-            {`\u00A0\u00A0\u00A0\u00A0`}
-            <Button
-              type='button'
-              className='btn btn_asLink'
-              onClick={() => {
-                history.push('/view');
-              }}
-            >
-              View Page
-            </Button>
-*/
